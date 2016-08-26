4ec63de23115d094c79dde8a3dab0dbed0fff70f
Kaira Villanueva <fzf.villanueva@gmail.com>
2016-08-26 13:18:22 -0400

    woo

---
+++ Air-Check Challenge/AeoluzUITests/AeoluzUITests.swift
@@ -0,0 +1,36 @@
+//
+//  AeoluzUITests.swift
+//  AeoluzUITests
+//
+//  Created by Kaira Villanueva on 8/18/16.
+//  Copyright © 2016 Kaira Villanueva. All rights reserved.
+//
+
+import XCTest
+
+class AeoluzUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
